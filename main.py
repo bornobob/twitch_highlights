7dc3f9ef4b984ea4121bde1faa4dca4b506a9cc4
Bob Ruiken <bobruiken@gmail.com>
2019-03-12 21:53:43 +0100

    Starting the twitch api binding

--- main.py
+++ main.py
@@ -35,6 +35,15 @@ year, month, day = 2019, 3, 11
 url = 'https://overrustlelogs.net/{0}%20chatlog/{1}%20{2}/{2}-{3}-{4}.txt'.format(streamer_name, MONTH_LIST[month-1],
                                                                                   year, str(month).zfill(2),
                                                                                   str(day).zfill(2))
+request_headers = {'Accept': 'application/vnd.twitchtv.v5+json', 'Client-ID': client_id}
+
+twitch_user_url = 'https://api.twitch.tv/kraken/users?login={}'.format(streamer_name)
+r = requests.get(twitch_user_url, headers=request_headers)
+streamer_id = r.json()['users'][0]['_id']
+
+twitch_vod_url = 'https://api.twitch.tv/kraken/channels/{}/videos?broadcasts=true'.format(streamer_id)
+r = requests.get(twitch_vod_url, headers=request_headers)
+print(r.json())
 
 response = requests.get(url)
 all_chats = response.text
