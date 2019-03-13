e362df5b38141a59a30d95b744eb59feb9e2374e
Bob Ruiken <bobruiken@gmail.com>
2019-03-13 19:18:15 +0100

    Added functioning VodEntry object

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import matplotlib.pyplot as plt
 import re
 import json
+from datetime import datetime, timedelta
 
 msg_re = re.compile(r'\[[\d]+-[\d]+-[\d]+ (?P<hrs>[\d]+):(?P<mins>[\d]+):(?P<secs>[\d]+) '
                     r'(?P<timezone>[a-zA-Z]+)\] (?P<author>[^:]+): (?P<msg>.*)')
@@ -29,7 +30,13 @@ class VodEntry:
         self.broadcast_id = broadcast_id
         self.url = url
         self.length = length
-        self.broadcast_at = broadcast_at
+        self.time_started = datetime.strptime(broadcast_at, '%Y-%m-%dT%H:%M:%SZ')
+        self.time_finished = self.time_started + timedelta(seconds=length)
+
+    def __str__(self):
+        return 'VOD_ID: {}, URL: {}, length: {}:{}:{}, from: {}, until: {}'.format(
+            self.broadcast_id, self.url, *secs_to_time(self.length), self.time_started.strftime('(%b %d) %H:%M'),
+            self.time_finished.strftime('(%b %d) %H:%M'))
 
 
 def secs_to_time(seconds):
@@ -44,9 +51,9 @@ def get_streamer_id(streamer_name):
     return r.json()['users'][0]['_id']
 
 
-def get_vods_by_streamer_id(streamer_id):
-    twitch_vod_url = 'https://api.twitch.tv/kraken/channels/{}/videos?broadcast_type=archive&limit=100'.format(
-        streamer_id)
+def get_vods_by_streamer_id(streamer_id, limit=100):
+    twitch_vod_url = 'https://api.twitch.tv/kraken/channels/{}/videos?broadcast_type=archive&limit={}'.format(
+        streamer_id, limit)
     r = requests.get(twitch_vod_url, headers=request_headers)
     jsondata = r.json()
     return jsondata['_total'], jsondata['videos']
@@ -54,7 +61,7 @@ def get_vods_by_streamer_id(streamer_id):
 
 def get_logs_url(streamer_name, year, month, day):
     return 'https://overrustlelogs.net/{0}%20chatlog/{1}%20{2}/{2}-{3}-{4}.txt'.format(streamer_name,
-                                                                                       MONTH_LIST[month-1], year,
+                                                                                       MONTH_LIST[month - 1], year,
                                                                                        str(month).zfill(2),
                                                                                        str(day).zfill(2))
 
@@ -72,19 +79,29 @@ def get_messages_by_log_text(log_text):
 
 
 def show_info(all_messages):
-    all_timestamps = [0]*(24*60*60)
+    all_timestamps = [0] * (24 * 60 * 60)
     for chat in all_messages:
-        all_timestamps[chat.hours*3600 + chat.minutes*60 + chat.seconds] += 1
-    print('Average number of messages in 24 hours:', sum(all_timestamps)/len(all_timestamps))
-    plt.plot(range(24*60*60), all_timestamps)
+        all_timestamps[chat.hours * 3600 + chat.minutes * 60 + chat.seconds] += 1
+    print('Average number of messages in 24 hours:', sum(all_timestamps) / len(all_timestamps))
+    plt.plot(range(24 * 60 * 60), all_timestamps)
     plt.show()
+
+
+def convert_raw_vods(raw_vods):
+    vods = []
+    for v in raw_vods:
+        vods.append(VodEntry(v['broadcast_id'], v['url'], v['length'], v['recorded_at']))
+    return vods
 
 
 streamer_name = 'loltyler1'
 year, month, day = 2019, 3, 12
 
 streamer_id = get_streamer_id(streamer_name)
-total, vods = get_vods_by_streamer_id(streamer_id)
+total, raw_vods = get_vods_by_streamer_id(streamer_id)
 logs_url = get_logs_url(streamer_name, year, month, day)
 log_text = get_log_text_by_url(logs_url)
 all_messages = get_messages_by_log_text(log_text)
+vods = convert_raw_vods(raw_vods)
+for v in vods[::-1]:
+    print(v)
