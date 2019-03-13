a99f04bba5bda6927ae3ed4c2c398a86553e8d88
Bob Ruiken <bobruiken@gmail.com>
2019-03-13 12:31:57 +0100

    Cleaned up code into functions

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import requests
 import matplotlib.pyplot as plt
 import re
-from time import time
-import statistics
+import json
 
 msg_re = re.compile(r'\[[\d]+-[\d]+-[\d]+ (?P<hrs>[\d]+):(?P<mins>[\d]+):(?P<secs>[\d]+) '
                     r'(?P<timezone>[a-zA-Z]+)\] (?P<author>[^:]+): (?P<msg>.*)')
+client_id = json.loads(open('credentials.json', 'r').read())['client_id']
+request_headers = {'Accept': 'application/vnd.twitchtv.v5+json', 'Client-ID': client_id}
+MONTH_LIST = ['January', 'February', 'March', 'April', 'May', 'June',
+              'July', 'August', 'September', 'October', 'November', 'December']
 
 
 class ChatEntry:
@@ -21,53 +24,67 @@ class ChatEntry:
         self.message = match['msg']
 
 
+class VodEntry:
+    def __init__(self, broadcast_id, url, length, broadcast_at):
+        self.broadcast_id = broadcast_id
+        self.url = url
+        self.length = length
+        self.broadcast_at = broadcast_at
+
+
 def secs_to_time(seconds):
     hours, seconds = divmod(seconds, 3600)
     minutes, seconds = divmod(seconds, 60)
     return hours, minutes, seconds
 
 
-MONTH_LIST = ['January', 'February', 'March', 'April', 'May', 'June',
-              'July', 'August', 'September', 'October', 'November', 'December']
+def get_streamer_id(streamer_name):
+    twitch_user_url = 'https://api.twitch.tv/kraken/users?login={}'.format(streamer_name)
+    r = requests.get(twitch_user_url, headers=request_headers)
+    return r.json()['users'][0]['_id']
+
+
+def get_vods_by_streamer_id(streamer_id):
+    twitch_vod_url = 'https://api.twitch.tv/kraken/channels/{}/videos?broadcast_type=archive&limit=100'.format(
+        streamer_id)
+    r = requests.get(twitch_vod_url, headers=request_headers)
+    jsondata = r.json()
+    return jsondata['_total'], jsondata['videos']
+
+
+def get_logs_url(streamer_name, year, month, day):
+    return 'https://overrustlelogs.net/{0}%20chatlog/{1}%20{2}/{2}-{3}-{4}.txt'.format(streamer_name,
+                                                                                       MONTH_LIST[month-1], year,
+                                                                                       str(month).zfill(2),
+                                                                                       str(day).zfill(2))
+
+
+def get_log_text_by_url(logs_url):
+    r = requests.get(logs_url)
+    return r.text
+
+
+def get_messages_by_log_text(log_text):
+    separated_msgs = []
+    for c in log_text.split('\n')[:-1]:
+        separated_msgs.append(ChatEntry(c))
+    return separated_msgs
+
+
+def show_info(all_messages):
+    all_timestamps = [0]*(24*60*60)
+    for chat in all_messages:
+        all_timestamps[chat.hours*3600 + chat.minutes*60 + chat.seconds] += 1
+    print('Average number of messages in 24 hours:', sum(all_timestamps)/len(all_timestamps))
+    plt.plot(range(24*60*60), all_timestamps)
+    plt.show()
+
 
 streamer_name = 'loltyler1'
-year, month, day = 2019, 3, 11
-url = 'https://overrustlelogs.net/{0}%20chatlog/{1}%20{2}/{2}-{3}-{4}.txt'.format(streamer_name, MONTH_LIST[month-1],
-                                                                                  year, str(month).zfill(2),
-                                                                                  str(day).zfill(2))
-request_headers = {'Accept': 'application/vnd.twitchtv.v5+json', 'Client-ID': client_id}
+year, month, day = 2019, 3, 12
 
-twitch_user_url = 'https://api.twitch.tv/kraken/users?login={}'.format(streamer_name)
-r = requests.get(twitch_user_url, headers=request_headers)
-streamer_id = r.json()['users'][0]['_id']
-
-twitch_vod_url = 'https://api.twitch.tv/kraken/channels/{}/videos?broadcasts=true'.format(streamer_id)
-r = requests.get(twitch_vod_url, headers=request_headers)
-print(r.json())
-
-response = requests.get(url)
-all_chats = response.text
-
-time_before = time()
-separated_msgs = []
-for chat in all_chats.split('\n')[:-1]:
-    separated_msgs.append(ChatEntry(chat))
-print('Loaded', len(separated_msgs), 'messages in', time()-time_before, 'ms')
-
-all_timestamps = [0]*(24*60*60)
-for chat in separated_msgs:
-    all_timestamps[chat.hours*3600 + chat.minutes*60 + chat.seconds] += 1
-
-print('Average number of messages in 24 hours:', sum(all_timestamps)/len(all_timestamps))
-
-streamed_timestamps = list(filter(lambda x: all_timestamps[x] >= 3, range(len(all_timestamps))))
-print('Time spent streaming: {}:{}:{}'.format(*secs_to_time(len(streamed_timestamps))))
-
-streamed_messages = list(map(lambda x: all_timestamps[x], streamed_timestamps))
-average_streaming_messages = sum(streamed_messages)/len(streamed_messages)
-print('Average number of messages in streaming time:', average_streaming_messages)
-print('Standard deviation of msgs in streaming time:', statistics.stdev(streamed_messages))
-
-plt.plot(range(24*60*60), all_timestamps)
-plt.plot(streamed_timestamps, [0]*len(streamed_timestamps), 'ro')
-plt.show()
+streamer_id = get_streamer_id(streamer_name)
+total, vods = get_vods_by_streamer_id(streamer_id)
+logs_url = get_logs_url(streamer_name, year, month, day)
+log_text = get_log_text_by_url(logs_url)
+all_messages = get_messages_by_log_text(log_text)
