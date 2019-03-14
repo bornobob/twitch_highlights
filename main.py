e521638f5dc6c84fe7a0d1e0b6454d45200dac38
Bob Ruiken <bobruiken@gmail.com>
2019-03-14 14:46:25 +0100

    Added stream extraction from vods
    Can detect short (variable) pauses inbetween streams.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import re
 import json
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 msg_re = re.compile(r'\[[\d]+-[\d]+-[\d]+ (?P<hrs>[\d]+):(?P<mins>[\d]+):(?P<secs>[\d]+) '
                     r'(?P<timezone>[a-zA-Z]+)\] (?P<author>[^:]+): (?P<msg>.*)')
@@ -48,27 +48,33 @@ class VodEntry:
             self.time_started.strftime('(%b %d) %H:%M'), self.time_finished.strftime('(%b %d) %H:%M'))
 
 
+class StreamEntry:
+    def __init__(self, vods):
+        self.vods = vods
+
+    def __str__(self):
+        return '-' * 20 + '\n' + '\n'.join(' - ' + str(x) for x in self.vods)
+
+
 class UnknownStreamerError(Exception):
     pass
 
 
 class TwitchHighligher:
-    def __init__(self, streamer_name, year, month, day):
+    def __init__(self, streamer_name, year, month, day, max_inter_stream_time=3600):
         self.streamer_name = streamer_name
         self.year = year
         self.month = month
         self.day = day
+        self.max_inter_stream_time = max_inter_stream_time
         self.streamer_id = self.get_streamer_id()
-        self.separated_messages = self.get_messages_by_log_text()
-        self.total_vods, self.raw_vods = self.get_vods_by_streamer_id()
-        self.vods = self.convert_raw_vods()
 
     def get_streamer_id(self):
         try:
             twitch_user_url = 'https://api.twitch.tv/kraken/users?login={}'.format(self.streamer_name)
             r = requests.get(twitch_user_url, headers=request_headers)
             return r.json()['users'][0]['_id']
-        except KeyError or IndexError:
+        except (KeyError, IndexError):
             raise UnknownStreamerError('The given streamer was not found on twitch', self.streamer_name)
 
     def get_vods_by_streamer_id(self, limit=100):
@@ -76,32 +82,93 @@ class TwitchHighligher:
             self.streamer_id, limit)
         r = requests.get(twitch_vod_url, headers=request_headers)
         jsondata = r.json()
-        return jsondata['_total'], jsondata['videos']
+        return jsondata['_total'], jsondata['videos'][::-1]
 
-    def get_logs_url(self):
+    def get_logs_url(self, year, month, day):
         return 'https://overrustlelogs.net/{0}%20chatlog/{1}%20{2}/{2}-{3}-{4}.txt'.format(self.streamer_name,
-                                                                                           MONTH_LIST[self.month - 1],
-                                                                                           self.year,
-                                                                                           str(self.month).zfill(2),
-                                                                                           str(self.day).zfill(2))
+                                                                                           MONTH_LIST[month - 1],
+                                                                                           year,
+                                                                                           str(month).zfill(2),
+                                                                                           str(day).zfill(2))
 
-    def get_log_text_by_url(self):
-        r = requests.get(self.get_logs_url())
+    def get_log_text_by_url(self, year, month, day):
+        r = requests.get(self.get_logs_url(year, month, day))
         return r.text
 
-    def get_messages_by_log_text(self):
+    def get_messages_by_log_text(self, year, month, day):
         separated_msgs = []
-        for c in self.get_log_text_by_url().split('\n')[:-1]:
+        for c in self.get_log_text_by_url(year, month, day).split('\n')[:-1]:
             separated_msgs.append(ChatEntry(c))
         return separated_msgs
 
-    def convert_raw_vods(self):
+    @staticmethod
+    def convert_raw_vods(raw_vods):
         vods = []
-        for v in self.raw_vods:
+        for v in raw_vods:
             vods.append(VodEntry(v['broadcast_id'], v['url'], v['length'], v['recorded_at']))
         return vods
 
+    def get_vods_on_date(self, vods):
+        given_date = datetime(self.year, self.month, self.day, tzinfo=timezone.utc).date()
+        relevant_vods = []
+        for v in vods:
+            if v.time_started.date() <= given_date <= v.time_finished.date():
+                relevant_vods.append(v)
+        return relevant_vods
 
-th = TwitchHighligher('loltyler1', 2019, 3, 12)
-for vod in th.vods[::-1]:
-    print(vod)
+    def get_same_stream_vods_before(self, index, vods):
+        added_vods = []
+        while index > 0:
+            difference = vods[index - 1].time_finished - vods[index].time_started
+            if abs(difference.total_seconds()) <= self.max_inter_stream_time:
+                added_vods.append(vods[index - 1])
+            else:
+                break
+            index -= 1
+        return added_vods[::-1]
+
+    def get_same_stream_vods_after(self, index, vods):
+        added_vods = []
+        while index < len(vods) - 1:
+            difference = vods[index + 1].time_started - vods[index].time_finished
+            if abs(difference.total_seconds()) <= self.max_inter_stream_time:
+                added_vods.append(vods[index + 1])
+            else:
+                break
+            index += 1
+        return added_vods
+
+    def get_relevant_stream_vods(self, vods, relevant_vods):
+        if relevant_vods:
+            before_same_stream_vods = self.get_same_stream_vods_before(vods.index(relevant_vods[0]), vods)
+            after_same_stream_vods = self.get_same_stream_vods_after(vods.index(relevant_vods[-1]), vods)
+            relevant_vods = before_same_stream_vods + relevant_vods + after_same_stream_vods
+        return relevant_vods
+
+    def get_stream_entries_from_vods(self, vods):
+        stream_entries, temp_stream = [], []
+        for v in vods:
+            if not temp_stream:
+                temp_stream.append(v)
+            else:
+                difference = v.time_started - temp_stream[-1].time_finished
+                if abs(difference.total_seconds()) <= self.max_inter_stream_time:
+                    temp_stream.append(v)
+                else:
+                    stream_entries.append(StreamEntry(temp_stream))
+                    temp_stream = [v]
+        if temp_stream:
+            stream_entries.append(StreamEntry(temp_stream))
+        return stream_entries
+
+    def get_highlights(self):
+        total_vods, raw_vods = self.get_vods_by_streamer_id()
+        vods = self.convert_raw_vods(raw_vods)
+        relevant_vods = self.get_relevant_stream_vods(vods, self.get_vods_on_date(vods))
+        stream_entries = self.get_stream_entries_from_vods(relevant_vods)
+        for s in stream_entries:
+            print(s)
+
+
+th = TwitchHighligher('loltyler1', 2019, 3, 13)
+th.get_highlights()
